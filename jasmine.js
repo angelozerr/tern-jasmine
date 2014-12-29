c1f37e767ff03adfccd447d31c5f2f097a3b4eee
angelozerr <angelo.zerr@gmail.com>
2014-12-29 17:40:05 +0100

    Remove tab

--- jasmine.js
+++ jasmine.js
@@ -16,12 +16,12 @@
   });
   
   var defs = {
-	"!name": "jasmine",	
-	"describe": {
-	  "!type": "fn(description: string, specDefinitions: fn())",
-	  "!doc": "A test suite begins with a call to the global Jasmine function describe with two parameters: a string and a function. The string is a name or title for a spec suite - usually what is being tested. The function is a block of code that implements the suite."
-	},
-	"it": {
+    "!name": "jasmine",	
+    "describe": {
+      "!type": "fn(description: string, specDefinitions: fn())",
+      "!doc": "A test suite begins with a call to the global Jasmine function describe with two parameters: a string and a function. The string is a name or title for a spec suite - usually what is being tested. The function is a block of code that implements the suite."
+    },
+    "it": {
       "!type": "fn(description: string, specDefinitions: fn())",
       "!doc": "Specs are defined by calling the global Jasmine function it, which, like describe takes a string and a function. The string is the title of the spec and the function is the spec, or test. A spec contains one or more expectations that test the state of the code. An expectation in Jasmine is an assertion that is either true or false. A spec with all true expectations is a passing spec. A spec with one or more false expectations is a failing spec."
     }
